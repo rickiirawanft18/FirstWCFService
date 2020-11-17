095802973d0536f1ddf3dcd4d159c93291824669
Ricki Irawan <ricki.irawan.ft18@mail.umy.ac.id>
2020-11-17 17:04:53 +0700

    Add project files.

---
+++ FirstWCFService/IService1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FirstWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        double add(double num1, double num2);
+
+        [OperationContract]
+        double Sub(double num1, double num2);
+
+        [OperationContract]
+        double Multipli(double num1, double num2);
+
+        [OperationContract]
+        double Divide(double num1, double num2);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+
+}
+
